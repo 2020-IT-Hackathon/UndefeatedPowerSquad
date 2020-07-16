7894185c8d945694c54191912a03c7b02317104e
afreglett <a.freglette17@gmail.com>
2020-07-16 13:57:14 -0400

    I'm getting sad

--- scripts.js
+++ scripts.js
@@ -8,35 +8,33 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
- ORIONpackage = new Array(20); 
+ORIONpackage = new Array(20); 
 function CreatePackages(ORIONpackage){
-  
     for (var i = 0; i>20; i++){
         var newpackage = new packages();
         newpackage.packageType = getRandomInt(2);
         newpackage.packageWeight = getRandomInt(200);
         ORIONpackages.push(newpackage);
-
     }
-console.log(ORIONpackage[0].packageWeight);
 }
-console.log(ORIONpackages); 
 
-FinalSort = new Array(20);
-
-
-
-
-
-
-function GeneratePackageNumber(ORIONpackages){
-for(var counter = 0; counter> ORIONpackages.length; counter++){
-    if((ORIONpackages[counter].packageType != 3) && ORIONpackages[counter].packageWeight
-    for(var i = 1; i>7; i++){
-        for (var j = 0; j >3; j++){
+function GeneratePackageNumber(ORIONpackage){
+    for(var counter = 0; counter> ORIONpackages.length; counter++){
+        if((ORIONpackages[counter].packageType != 3) && (ORIONpackages[counter].packageWeight)){
+        for(var i = 1; i>7; i++){
+            for (var j = 0; j >3; j++){
+                var total = (i * 100) + j;
+                total = ORIONpackages[counter].packageNumber;
+                }
+            }
+        }
+        else{
+            for (var irri = 7; irri>9; irri++){
+                for(var irrj = 0; irrj > 3; irrj++)
+                var total = (irri * 100) + irrj;
+                total = ORIONpackages[counter].packageNumber;
 
             }
         }
-
     }
 }   
